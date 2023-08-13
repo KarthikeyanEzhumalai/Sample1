9e65ed22d1ff3666462fd736f74006ad800e81a0
KarthikeyanEzhumalai <karthikjks513@gmail.com>
2023-08-13 22:53:12 +0530

    Sample1 Project Coding Completed

---
+++ src/org/com/DailyTaskDone.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class DailyTaskDone {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "java";
+		
+		String s2 = "Java";
+		
+		boolean checkEqual = s1.equals(s2);
+		
+		System.out.println(checkEqual);
+		
+		
+		
+	}
+
+}
